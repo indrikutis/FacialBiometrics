29875d7f75a31db92ab0c8a103bf760ad860b93f
Indre Aruodziute <s222958@dtu.dk>
2024-01-23 13:53:17 +0100

    FGNET tool analysis results

--- Accuracy_analysis.py
+++ Accuracy_analysis.py
@@ -14,26 +14,35 @@ def convert_race_label(race_label):
 
 def convert_race4_label(race_label):
     gender_mapping = {'Man': 'Male', 'Woman': 'Female'}
-    return gender_mapping.get(gender_label, 'Other')
+    return gender_mapping.get(race_label, 'Other')
 
 
 # Function to create a merged dataframe from DeepFace results and UTKFace dataset
-def create_merged_df(tool_results, utkface_dataset, tool, merged_df_name):
+def create_merged_df(tool_results, dataset, tool, merged_df_name):
+
+    print(tool_results)
+    print(dataset)
 
     if (tool).lower() == 'deepface':
-        # Convert DeepFace race labels to UTKFace race labels
-        tool_results['race4'] = tool_results['race4'].apply(convert_race4_label)
 
-        # Convert DeepFace gender labels to UTKFace gender labels
-        tool_results['Gender'] = tool_results['Gender'].apply(convert_gender_label)
+        if 'Gender' in dataset.columns:
+            # Convert DeepFace gender labels to UTKFace gender labels
+            tool_results['Gender'] = tool_results['Gender'].apply(convert_gender_label)
 
 
-        # Merge DeepFace results with UTKFace dataset based on image names
-        merged_df = pd.merge(tool_results, utkface_dataset, how='inner', left_on='Image name', right_on='image_name')
+        if 'Image name' in tool_results.columns and 'image_name' in dataset.columns:
+            merged_df = pd.merge(tool_results, dataset, how='inner', left_on='Image name', right_on='image_name')
+        else:
+            merged_df = pd.merge(tool_results, dataset, how='inner', on='image_name')
+
+
         merged_df = merged_df[merged_df['Notes_x'].isna() & merged_df['Notes_y'].isna()]
 
     else:
-        merged_df = pd.merge(tool_results, utkface_dataset, how='inner', left_on='image_name', right_on='image_name')
+        # Convert DeepFace race labels to UTKFace race labels
+        tool_results['race4'] = tool_results['race4'].apply(convert_race4_label)
+
+        merged_df = pd.merge(tool_results, dataset, how='inner', left_on='image_name', right_on='image_name')
         merged_df = merged_df[merged_df['Notes'].isna()]
 
     results_folder = "Tool analysis"
@@ -54,6 +63,63 @@ def extract_age_bounds(age_group, age_range):
         return max(0, lower - age_range), upper + age_range # Ensure lower bound is not negative
     else:
         return int(age_group), int(age_group)
+
+def calculate_accuracy_FGNET(merged_df, age_range, tool):
+
+    if (tool).lower() == 'deepface':
+
+        # No need to calculate gender accuracy
+
+        age_accuracy = (merged_df['Age'] == merged_df['age']).mean()
+        age_error_rate = 1 - age_accuracy
+
+        # Age accuracy within a range since predicting the exact age is difficult
+        age_within_range = (
+            (merged_df['Age'] - age_range <= merged_df['age']) &
+            (merged_df['Age'] + age_range >= merged_df['age'])
+        )
+
+        print(age_within_range)
+
+        age_accuracy_within_range = age_within_range.mean()
+        age_error_rate_within_range = 1 - age_accuracy_within_range
+
+    else: 
+
+        # No need to calculate gender accuracy
+        
+        # Extract lower and upper bounds from the predicted age range
+        merged_df[['predicted_age_lower', 'predicted_age_upper']] = merged_df['age_group'].apply(lambda x: extract_age_bounds(x, 0)).apply(pd.Series)
+
+        # Check if the true age falls within the predicted range
+        age_accuracy_range = (
+            (merged_df['age'] >= merged_df['predicted_age_lower']) &
+            (merged_df['age'] <= merged_df['predicted_age_upper'])
+        )
+
+        # Calculate accuracy within the predicted range
+        age_accuracy = age_accuracy_range.mean()
+        age_error_rate = 1 - age_accuracy
+
+        merged_df[['predicted_age_lower', 'predicted_age_upper']] = merged_df['age_group'].apply(lambda x: extract_age_bounds(x, int(age_range/2))).apply(pd.Series)
+
+        # Check if the true age falls within the predicted range
+        age_accuracy_range = (
+            (merged_df['age'] >= merged_df['predicted_age_lower']) &
+            (merged_df['age'] <= merged_df['predicted_age_upper'])
+        )
+
+        # Calculate accuracy within the predicted range
+        age_accuracy_within_range = age_accuracy_range.mean()
+        age_error_rate_within_range = 1 - age_accuracy_within_range
+
+
+    # Print results
+    print(f"Age Accuracy: {age_accuracy * 100:.2f}%")
+    print(f"Age Error Rate: {age_error_rate * 100:.2f}%\n")
+    print(f"Age Accuracy within {age_range} years: {age_accuracy_within_range * 100:.2f}%")
+    print(f"Ages Error Rate within {age_range} years: {age_error_rate_within_range * 100:.2f}%\n")
+
 
 def calculate_accuracy(merged_df, age_range, tool):
 
@@ -80,8 +146,6 @@ def calculate_accuracy(merged_df, age_range, tool):
         gender_error_rate = 1 - gender_accuracy
         
         # Extract lower and upper bounds from the predicted age range
-        # predicted_age_lower = merged_df['age_group'].apply(lambda x: int(str(x).split('-')[0]) if isinstance(x, str) and '-' in x else int(str(x).split('.')[0]))
-        # predicted_age_upper = merged_df['age_group'].apply(lambda x: int(x.split('-')[1]) if isinstance(x, str) and '-' in x else int(str(x).split('.')[0]))
         merged_df[['predicted_age_lower', 'predicted_age_upper']] = merged_df['age_group'].apply(lambda x: extract_age_bounds(x, 0)).apply(pd.Series)
 
         # Check if the true age falls within the predicted range
@@ -179,14 +243,17 @@ def analyse_confusion_matrix(merged_df, tool):
 
 
 # Load the DeepFace results and UTKFace datasets
-deepface_results = pd.read_excel("Results/FairFace_analysis_results_UTKFace_0.3.xlsx")
-utkface_dataset = pd.read_excel("UTKFace/UTKFace_dataset_info.xlsx")
-merged_df_name = "UTKface_part1_0.3_sampling_opencv_merged_results.xlsx"
+analysis_results = pd.read_excel("Results/FairFace_analysis_results_FGNET.xlsx")
+dataset_info = pd.read_excel("Dataset_info/FGNET_dataset_info.xlsx")
+merged_df_name = "FGNET_FairFace_merged_results.xlsx"
 age_range = 5
 tool = 'FairFace'
 
-merged_df = create_merged_df(deepface_results, utkface_dataset, tool, merged_df_name)
-calculate_accuracy(merged_df, age_range, tool)
-demographic_distribution(merged_df)
-model_predictions_by_race(merged_df, tool)
-analyse_confusion_matrix(merged_df, tool)
+merged_df = create_merged_df(analysis_results, dataset_info, tool, merged_df_name)
+
+# calculate_accuracy(merged_df, age_range, tool)
+calculate_accuracy_FGNET(merged_df, age_range, tool)
+
+# demographic_distribution(merged_df)
+# model_predictions_by_race(merged_df, tool)
+# analyse_confusion_matrix(merged_df, tool)
